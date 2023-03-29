9a500b6d7221c658f1af683be48d4cb480e8e388
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-29 11:32:35 -0300

    refactor: message displayNameFormat

--- back-end/src/utils/messages.js
+++ back-end/src/utils/messages.js
@@ -1,4 +1,4 @@
-const displayNameFormat = '"displayName" length must be at least 8 characters long';
+const displayNameFormat = '"name" length must be at least 8 characters long';
 const emailFormat = '"email" must be a valid email';
 const passwordFormat = '"password" length must be at least 6 characters long';
 
