9d70e7e76add68cb860da49908639416dca0cad9
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-29 17:50:18 -0300

    feat: cria rota "/login" no front e redireciona rota "/" para "/login"

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import FormProvider from './context/FormProvider';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <main className="App">
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
+      <FormProvider>
+        <Switch>
+          <Route exact path="/login" component={ Login } />
+        </Switch>
+      </FormProvider>
+    </main>
   );
 }
 
