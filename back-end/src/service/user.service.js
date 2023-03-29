675d74d0cf8df2f7d1e07d6188bbb692722043b1
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-29 11:13:57 -0300

    feat: camada service referente a rota de criação de usuário

---
+++ back-end/src/service/user.service.js
@@ -0,0 +1,28 @@
+const md5 = require('md5');
+const { User } = require('../database/models');
+const { generateToken } = require('../utils/generate.token');
+
+const createUser = async ({ name, email, password, role }) => {
+  const emailExists = await User.findOne({ where: { email } });
+  const nameExists = await User.findOne({ where: { name } });
+
+  if (emailExists || nameExists) return ({ status: 409, message: 'User already registered' });
+
+  const cryptoPassword = md5(password);
+
+  let type = role;
+
+  if (!role) {
+    type = 'customer';
+  }
+
+  const user = await User.create({ name, email, password: cryptoPassword, role: type });
+
+  const token = generateToken({ email, role: user.role, id: user.id });
+
+  const result = { id: user.id, name, email, role: user.role, token };
+
+  return { status: 201, result };
+};
+
+module.exports = { createUser };
