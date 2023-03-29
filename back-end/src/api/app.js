82084e3f64308336806437fc892763fdd0779da9
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-29 17:47:53 -0300

    feat: adiciona o cors no backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('../routes/user.routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/user', userRoute);
 
