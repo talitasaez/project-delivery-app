0d1fe50e7030b62361bb200e7d1b6e95c1541ddf
Malrus-dev <maelsobfer@gmail.com>
2023-04-01 01:02:44 -0300

    feat: insere a rota sales no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const productsRoutes = require('../routes/productsRoutes');
-
+const salesRoutes = require('../routes/sale.routes');
 const userRoute = require('../routes/user.routes');
 
 const app = express();
@@ -10,6 +10,7 @@ app.use(express.json());
 
 app.use('/user', userRoute);
 app.use('/products', productsRoutes);
+app.use('/sales', salesRoutes);
 app.use('/images', express.static(`${__dirname}/images`));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
